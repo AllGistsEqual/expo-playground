f9e6b3301cb62b2cb153d0080a2f5f119c5ea77f
Konrad Abe <kae@dasburo.com>
2020-01-23 10:07:04 +0100

    added base styles for sides and corners and location based variations

--- src/components/Box/index.jsx
+++ src/components/Box/index.jsx
@@ -20,6 +20,20 @@ const Box = ({ children, setup }) => {
         SE = { id: 'SE' },
         SW = { id: 'SW' },
     } = setup
+    const base = StyleSheet.create({
+        pos: {
+            position: 'absolute',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 40,
+            height: 40,
+            backgroundColor: '#ccc',
+            borderRadius: 4,
+            borderWidth: 0.5,
+            borderColor: '#ff7100',
+        },
+    })
     const styles = StyleSheet.create({
         boxOuter: {
             width: width || 240,
@@ -33,16 +47,43 @@ const Box = ({ children, setup }) => {
             borderWidth: 0.5,
             borderColor: '#000',
         },
-        temp: {
-            width: 40,
-            height: 40,
-            backgroundColor: '#ccc',
-            borderRadius: 4,
-            borderWidth: 0.5,
-            borderColor: '#ff7100',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
+        posN: {
+            ...base.pos,
+            alignSelf: 'center',
+            top: 0,
+        },
+        posS: {
+            ...base.pos,
+            alignSelf: 'center',
+            bottom: 0,
+        },
+        posE: {
+            ...base.pos,
+            right: 0,
+        },
+        posW: {
+            ...base.pos,
+            left: 0,
+        },
+        posNE: {
+            ...base.pos,
+            top: 0,
+            right: 0,
+        },
+        posSE: {
+            ...base.pos,
+            bottom: 0,
+            right: 0,
+        },
+        posSW: {
+            ...base.pos,
+            bottom: 0,
+            left: 0,
+        },
+        posNW: {
+            ...base.pos,
+            top: 0,
+            left: 0,
         },
     })
 
@@ -52,7 +93,7 @@ const Box = ({ children, setup }) => {
                 [N, E, S, W, NE, NW, SE, SW].map((loc) => (
                     <View
                         key={loc.id}
-                        style={styles.temp}
+                        style={styles[`pos${loc.id}`]}
                     >
                         <Text>{loc.id}</Text>
                     </View>
