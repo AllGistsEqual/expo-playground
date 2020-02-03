50b129f3dca7f89333a69e86e1b85d3fe04c15ef
Konrad Abe <kae@dasburo.com>
2020-02-03 16:29:01 +0100

    additional cleanup for next tutorial

--- src/scenes/AppLoadingScreen.jsx
+++ src/scenes/AppLoadingScreen.jsx
@@ -2,25 +2,21 @@ import React from 'react'
 import { Text, View, TouchableWithoutFeedback } from 'react-native'
 import styles from '../styles'
 
-const ClickableScreen = () => (
-    <View style={styles.page}>
-        <View style={styles.titleBox}>
-            <Text>ALL BITS EQUAL</Text>
-            <Text>presents</Text>
-            <Text>The Expo Starter Kit</Text>
-        </View>
-        <View style={styles.contentBox}>
-            <Text>Touch Screen to start!</Text>
-        </View>
-        <View style={styles.footer}>
-            <Text>written by Konrad Abe</Text>
-        </View>
-    </View>
-)
-
 const AppLoadingScreen = () => (
     <TouchableWithoutFeedback>
-        <ClickableScreen />
+        <View style={styles.page}>
+            <View style={styles.titleBox}>
+                <Text>ALL BITS EQUAL</Text>
+                <Text>presents</Text>
+                <Text>The Expo Starter Kit</Text>
+            </View>
+            <View style={styles.contentBox}>
+                <Text>Touch Screen to start!</Text>
+            </View>
+            <View style={styles.footer}>
+                <Text>written by Konrad Abe</Text>
+            </View>
+        </View>
     </TouchableWithoutFeedback>
 )
 
